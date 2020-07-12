dc6ca91b264ed65d87953f4bf19bf0c37894c7b4
b05902132 <b05902132@csie.ntu.edu.tw>
2020-07-12 16:56:27 +0800

    Assignment 3

--- assign3/program/src/karel_impl.ml
+++ assign3/program/src/karel_impl.ml
@@ -4,7 +4,7 @@ open Option.Monad_infix
 exception Unimplemented
 
 (* Set this to true to print out intermediate state between Karel steps *)
-let debug = false
+let debug = true
 
 type cell =
   | Empty
@@ -38,13 +38,38 @@ let set_cell (grid : grid) ((i, j) : pos) (cell : cell) : grid =
 ;;
 
 let state_to_string (state : state) : string =
-  raise Unimplemented
+  let cell_symbol: cell -> char = fun s -> match s with
+    | Empty -> '.'
+    | Beeper -> 'B'
+    | Wall -> 'x'
+  in
+  let symbol_grid : char list list = List.map state.grid ~f:(List.map ~f:cell_symbol) in
+  let karel_grid: char list list = List.mapi symbol_grid
+      ~f:(fun (y:int) row -> (List.mapi row ~f:(fun (x:int) (c:char) -> if (x,y) = state.karel_pos then 'K' else c)) )
+  in
+  let row_list : string list = List.map karel_grid ~f:String.of_char_list in
+  List.fold_left row_list ~init:"" ~f:(fun l r -> (l ^ r) ^ ("\n"))
 ;;
+
 
 let empty_grid (m : int) (n : int) : grid =
   List.map (List.range 0 m) ~f:(fun _ ->
     List.map (List.range 0 n) ~f:(fun _ -> Empty))
 ;;
+
+let relative_to : pos -> dir -> pos = 
+  fun (x, y) dir -> match dir with
+    | North -> (x, y-1)
+    | South -> (x, y+1)
+    | East -> (x+1, y)
+    | West -> (x-1, y)
+
+let rotate_counterclockwise : dir -> dir = 
+  fun d -> match d with
+    | North -> West
+    | West -> South
+    | South -> East
+    | East -> North
 
 type predicate =
   | FrontIs of cell
@@ -95,11 +120,42 @@ and instruction_list_to_string (instrs: instruction list) : string =
 
 
 let rec eval_pred (state : state) (pred : predicate) : bool =
-  raise Unimplemented
-
+  match pred with
+  | Not p -> (eval_pred state p) <> true
+  | Facing d -> state.karel_dir = d
+  | FrontIs c -> (match get_cell state.grid (relative_to state.karel_pos state.karel_dir) with
+    | None -> c = Wall
+    | (Some d) -> c = d
+    )
+  | NoBeepersPresent -> (match get_cell state.grid state.karel_pos with
+    | Some c -> c <> Beeper
+    | None -> false
+    )
 
 let rec step (state : state) (code : instruction) : state =
-  raise Unimplemented
+  match code with 
+  | Move -> 
+    (let target_pos = relative_to state.karel_pos state.karel_dir in
+     match get_cell state.grid target_pos with
+     | None -> state
+     | Some Wall -> state
+     | _ -> {state with karel_pos = target_pos }
+    )
+  | TurnLeft -> {state with karel_dir = rotate_counterclockwise state.karel_dir}
+  | PickBeeper -> 
+    (match get_cell state.grid state.karel_pos with
+     | Some Beeper -> {state with grid = set_cell state.grid state.karel_pos Empty}
+     | _ -> state
+    )
+  | PutBeeper ->
+    (match get_cell state.grid state.karel_pos with
+     | Some Empty -> {state with grid = set_cell state.grid state.karel_pos Beeper}
+     | _ -> state
+    )
+  | While (p, instrs) -> if eval_pred state p 
+    then (let new_state = step_list state instrs in step new_state (While (p, instrs)) )
+    else state
+  | If (p, then_, else_) -> if eval_pred state p then step_list state then_ else step_list state else_
 
 and step_list (state : state) (instrs : instruction list) : state =
   List.fold instrs ~init:state ~f:(fun state instr ->
@@ -117,4 +173,17 @@ and step_list (state : state) (instrs : instruction list) : state =
 ;;
 
 let checkers_algo : instruction list = [
+  While (Not (FrontIs Wall),  [
+      PutBeeper;
+      While (Not (FrontIs Wall), [Move; If (FrontIs Wall, [], [Move;PutBeeper]) ]);
+      If (Facing East, [TurnLeft; TurnLeft; TurnLeft], [TurnLeft] );
+      (* currently facing south *)
+      If (FrontIs Wall, [], [
+          If (NoBeepersPresent, [
+              Move; TurnLeft; If (FrontIs Wall, [TurnLeft; TurnLeft;], [])
+            ], [ (* else *)
+              Move; TurnLeft; If (FrontIs Wall, [TurnLeft; TurnLeft;], []); Move
+            ])
+        ])
+  ])
 ]
--- assign3/program/src/ngram_impl.ml
+++ assign3/program/src/ngram_impl.ml
@@ -1,28 +1,46 @@
 open Core
 
 exception Unimplemented
+exception Impossible
 
 type ngram = string list
 type ngram_map = (ngram, string list) Map.Poly.t
 type word_distribution = float String.Map.t
 
 let rec remove_last_impl1 (l : string list) : string list =
-  raise Unimplemented
+  match l with
+  | [] -> []
+  | [x] -> []
+  | x::xs -> x :: (remove_last_impl1 xs)
 ;;
 
 assert (remove_last_impl1 ["a"; "b"] = ["a"]);
 ;;
 
 let remove_last_impl2 (l : string list) : string list =
-  raise Unimplemented
+  let len = List.length l in
+  List.filteri ~f:(fun i _ -> i + 1 <> len) l
 ;;
 
-assert (remove_last_impl2 ["a"; "b"] = ["a"])
+assert (remove_last_impl2 ["a"; "b"] = ["a"]);
+;;
+
+let rec split_last (l: 'a list) : 'a list * 'a option = 
+  match l with
+  | [] -> ([], None)
+  | [x] -> ([], Some x)
+  | x::xs -> let (ys, last) = split_last xs in (x::ys, last)
 ;;
 
 let compute_ngrams (l : string list) (n : int) : string list list =
-  raise Unimplemented
+  (* [a; b; c] -> [ []; [c]; [c;b]; [c;b;a] ] *)
+  let (_, rev_history) = List.fold_left l ~init:([], [[]])
+      ~f:(fun (ngram, accu) word -> let new_ngram = List.take (word::ngram) n in (new_ngram, new_ngram::accu))
+  in
+  let history : string list list = List.fold_left rev_history ~init:[] ~f:(fun accu ngram -> (List.rev ngram)::accu) in
+  List.drop history n
 ;;
+
 
 assert (compute_ngrams ["a"; "b"; "c"] 2 = [["a"; "b"]; ["b"; "c"]]);
 ;;
@@ -36,19 +54,43 @@ let ngram_map_new () : ngram_map =
 ;;
 
 let ngram_map_add (map : ngram_map) (ngram : ngram) : ngram_map =
-  raise Unimplemented
+  let (key, value) = split_last ngram in
+  let add (map : ngram_map) (key: ngram) (value: string) =
+    match Map.Poly.find map key with
+    | None -> Map.Poly.set map ~key:key ~data:[value]
+    | Some l -> Map.Poly.set map ~key:key ~data:(value::l)
+  in
+  match value with
+  | None -> map
+  | Some(value) -> add map key value 
 ;;
 
 let () =
   let map = ngram_map_new () in
-  let map = ngram_map_add map ["a"; "b"] in
-  (* Add your own tests here! *)
-  ()
+  let map = ngram_map_add map ["a"; "b"; "c"] in
+  assert (Map.Poly.find map ["a"; "b"] = Some ["c"] );
+  assert (Map.Poly.find map ["a"; "c"] = None );
+  let map = ngram_map_add map ["a"; "b"; "d"] in
+  assert (Map.Poly.find map ["a"; "b"] = Some ["d"; "c"] );
+  let map = ngram_map_add map ["a"; "b"; "d"] in
+  assert (Map.Poly.find map ["a"; "b"] = Some ["d"; "d"; "c"] );
+  let map = ngram_map_add map ["b"; "b"; "d"] in
+  assert (Map.Poly.find map ["a"; "b"] = Some ["d"; "d"; "c"] );
+  assert (Map.Poly.find map ["b"; "b"] = Some ["d"] );
+;;
+
+let calc_distribution (l: string list) : word_distribution = 
+  let alist : (string * unit) list = List.map l ~f:(fun x -> (x, ()) ) in
+  let word_count : int String.Map.t = String.Map.of_alist_fold alist ~init:0 ~f:(fun x _ -> x+1) in
+  let total_count = List.length l in
+  String.Map.map word_count ~f:(fun x -> (Float.of_int x) /. (Float.of_int total_count) )
 ;;
 
 let ngram_map_distribution (map : ngram_map) (ngram : ngram)
   : word_distribution option =
-  raise Unimplemented
+  match Map.Poly.find map ngram with
+  | None -> None
+  | Some ngram -> Some (calc_distribution ngram)
 ;;
 
 let distribution_to_string (dist : word_distribution) : string =
@@ -56,9 +98,21 @@ let distribution_to_string (dist : word_distribution) : string =
 ;;
 
 let sample_distribution (dist : word_distribution) : string =
-  raise Unimplemented
+  let rand = Random.float 1. in
+  let step ~key ~data (num, res) = match res with
+    | Some k -> (num, Some k)
+    | None -> let new_num = num -. data in if new_num <=. 0. then (0., Some key) else (new_num, None)
+  in
+  let (_, Some res) = String.Map.fold dist ~init:(rand, None) ~f:step in
+  res
 ;;
 
 let rec sample_n (map : ngram_map) (ng : ngram) (n : int) : string list =
-  raise Unimplemented
+  if n <= 0 then [] else
+    let (Some word_freq) = ngram_map_distribution map ng in
+    let word = sample_distribution word_freq in
+    let (_::tail) = ng in
+    let new_ng = tail @ [word] in
+    word :: sample_n map new_ng (n-1)
+    
 ;;
